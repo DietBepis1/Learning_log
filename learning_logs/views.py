7f6c4ec61c4ac5d1e0ee81df712470a41a2ea97d
Glenn Piludu <gpiludu@gmail.com>
2020-04-29 20:46:41 -0500

    started adding user functionality

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 def index(request):
@@ -11,3 +12,26 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Show a single topic and its entries"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Adds a new topic"""
+    if request.method != 'POST':
+        #No data submitted; create a blank form
+        form = TopicForm()
+    else:
+        #Post data submitted/process data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    
+    #Display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
