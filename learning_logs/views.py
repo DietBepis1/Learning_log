cb59f25f1332091261f2e7f5aac5ada2f79a3b04
Glenn Piludu <gpiludu@gmail.com>
2020-05-14 22:25:09 -0500

    added more user functions

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
@@ -7,12 +8,15 @@ def index(request):
     """Home page for learning log"""
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
     """Shows list of all topics"""
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Show a single topic and its entries"""
     topic = Topic.objects.get(id=topic_id)
@@ -20,6 +24,7 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Adds a new topic"""
     if request.method != 'POST':
@@ -36,6 +41,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Adds new entry for associated topic"""
     topic = Topic.objects.get(id=topic_id)
@@ -56,6 +62,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry"""
     entry = Entry.objects.get(id=entry_id)
